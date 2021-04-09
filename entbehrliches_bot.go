cd424f39dba73448eae33b21cdc83be13aad204d
Florian Baumann <flo@noqqe.de>
2021-04-09 15:06:13 +0200

    check if article exists

--- entbehrliches_bot.go
+++ entbehrliches_bot.go
@@ -3,29 +3,73 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 	"time"
+
+	"mvdan.cc/xurls/v2"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-var wikiurls = []string{
-	"://de.wikipedia.org/wiki",
-	"://de.m.wikipedia.org/wiki",
-	"://en.wikipedia.org/wiki",
-	"://en.m.wikipedia.org/wiki",
-}
+// Checks if message contains wiki url
+func containsWikiURL(msg string) (string, bool) {
 
-func containsWikiURL(msg string) bool {
-	var contains bool = false
+	var wikiurls = []string{
+		"://de.wikipedia.org/wiki",
+		"://de.m.wikipedia.org/wiki",
+		"://en.wikipedia.org/wiki",
+		"://en.m.wikipedia.org/wiki",
+	}
+
 	for _, s := range wikiurls {
 		if strings.Contains(msg, s) {
-			contains = true
+			rxStrict := xurls.Strict()
+			return rxStrict.FindString(msg), true
 		}
 	}
-	return contains
+	return "", false
+}
+
+// Generates a list of all markdown files
+func findMDFiles(root string) []string {
+	var matches []string
+
+	d, err := os.Open(root)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer d.Close()
+
+	files, err := d.Readdir(-1)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	for _, file := range files {
+		if file.Mode().IsRegular() {
+			if filepath.Ext(file.Name()) == ".md" {
+				matches = append(matches, root+"/"+file.Name())
+			}
+		}
+	}
+	return matches
+}
+
+// Searches a single markdown file for wiki url occourences
+// Returns slice [http.., http...]
+func searchFileforWikiLink(f string) []string {
+	fileBytes, err := ioutil.ReadFile(f)
+	if err != nil {
+		fmt.Println(err)
+	}
+	rxStrict := xurls.Strict()
+	return rxStrict.FindAllString(string(fileBytes), -1)
 }
 
 func main() {
@@ -33,6 +77,7 @@ func main() {
 	// Arguments
 	var (
 		apiToken = flag.String("apitoken", "", "Telegram API Token")
+		posts    = flag.String("posts", "", "Directory containing entbehrliches posts")
 	)
 	flag.Parse()
 
@@ -52,10 +97,39 @@ func main() {
 		return
 	}
 
+	// Find all files
+	log.Println("Finding all markdown files")
+	files := findMDFiles(*posts)
+
+	// var existing_wikilinks []string
+	var existing_urls []string
+
+	log.Println("Loading all urls")
+	for _, f := range files {
+		//existing_wikilinks = append(existing_wikilinks, searchFileforWikiLink(f)[0])
+		existing_urls = append(existing_urls, searchFileforWikiLink(f)...)
+	}
+
+	log.Println("Starting Telegram Bot")
 	b.Handle(tb.OnText, func(m *tb.Message) {
-		fmt.Println(m.Text)
-		if containsWikiURL(m.Text) {
-			b.Send(m.Chat, "Wiki url gefunden")
+
+		var count int = 0
+		url, containsurl := containsWikiURL(m.Text)
+		if containsurl {
+
+			log.Printf("Searching for %s in archives...\n", url)
+			for _, s := range existing_urls {
+				if strings.Contains(s, url) {
+					count = count + 1
+					log.Printf("Found %s in articles.\n", url)
+				}
+			}
+
+			if count > 0 {
+				b.Send(m.Chat, "Hatten wa schon!")
+			} else {
+				b.Send(m.Chat, "Cool, kennsch garnet")
+			}
 		}
 	})
 
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.16
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	gopkg.in/tucnak/telebot.v2 v2.3.5
+	mvdan.cc/xurls/v2 v2.2.0
 )
